798bcd17b51e147ccc25f1d78ad28378ff7d08dd
3sara <84321374+3sara@users.noreply.github.com>
2023-12-14 19:16:42 +0100

    inizio di data-preparation.R

--- code/data-preparation/data-preparation.R
+++ code/data-preparation/data-preparation.R
@@ -1,5 +1,16 @@
 # R code for the data preparation phase
-tweets_df=read.csv2("../../datasets/tweets&users-data.csv",header=T,sep=',')
+df<-read.csv("dataset.csv")
+# converti queste colonne in binario
+df$external.link <- ifelse(grepl("^\\s*$",df$external.link), 0, 1)
+df$pictures <- ifelse(grepl("^\\[\\]$",df$pictures), 0, 1)
+df$videos<-ifelse(grepl("^\\[\\]$",df$videos), 0, 1)
+df$gifs<-ifelse(grepl("^\\[\\]$",df$gifs), 0, 1)
+
+# 1 se il tweet contiene almeno un'immagine, video o gifs
+df$multimedial_content <- df$picture| df$videos|df$gifs
+df$multimedial_content<-as.integer((df$multimedial_content))
+
+tweets_df <- subset(df, select = -c(pictures, videos,gifs))
 
 # TO DO
 #   - compute eng-rate
