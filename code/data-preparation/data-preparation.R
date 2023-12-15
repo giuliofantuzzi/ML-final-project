e5ee73afd7afd3c2493a0a23291a1e68072ffcaf
giuliofantuzzi <giulio.fantuzzi01@gmail.com>
2023-12-15 21:28:52 +0100

    finished web scraping and data preparation

--- code/data-preparation/data-preparation.R
+++ code/data-preparation/data-preparation.R
@@ -1,20 +1,39 @@
+#-------------------------------------------------------------------------------
 # R code for the data preparation phase
-df<-read.csv("dataset.csv")
-# converti queste colonne in binario
-df$external.link <- ifelse(grepl("^\\s*$",df$external.link), 0, 1)
-df$pictures <- ifelse(grepl("^\\[\\]$",df$pictures), 0, 1)
-df$videos<-ifelse(grepl("^\\[\\]$",df$videos), 0, 1)
-df$gifs<-ifelse(grepl("^\\[\\]$",df$gifs), 0, 1)
+#-------------------------------------------------------------------------------
 
-# 1 se il tweet contiene almeno un'immagine, video o gifs
-df$multimedial_content <- df$picture| df$videos|df$gifs
-df$multimedial_content<-as.integer((df$multimedial_content))
+# Import data
+tweets_df<-read.csv("../../datasets/tweets&users-data-merged.csv")
 
-tweets_df <- subset(df, select = -c(pictures, videos,gifs))
+# Convert some variables to binary
+tweets_df$external.link <- ifelse(grepl("^\\s*$",tweets_df$external.link), FALSE, TRUE)
+tweets_df$pictures <- ifelse(grepl("^\\[\\]$",tweets_df$pictures), FALSE, TRUE)
+tweets_df$videos<-ifelse(grepl("^\\[\\]$",tweets_df$videos), FALSE, TRUE)
+tweets_df$gifs<-ifelse(grepl("^\\[\\]$",tweets_df$gifs), FALSE, TRUE)
+
+tweets_df$is.retweet<-ifelse(grepl("False",tweets_df$is.retweet), FALSE, TRUE)
+
+tweets_df$user.image<-ifelse(tweets_df$user.image == "", FALSE, TRUE)
+tweets_df$user.bio<-ifelse(tweets_df$user.bio == "", FALSE, TRUE)
+tweets_df$user.website<-ifelse(tweets_df$user.website == "", FALSE, TRUE)
+
+# Binary variable which is true if the tweet contains ANY 
+tweets_df$multimedial_content <- tweets_df$picture| tweets_df$videos | tweets_df$gifs
+
+# Compute engagement rate
+tweets_df$engagement.rate= with(tweets_df,(likes+retweets+comments)/(1+user.followers)*100)
+
+# Save csv 
+sub= subset(tweets_df,select= c("text","quotes","is.retweet","external.link",
+                                "pictures","videos","gifs","multimedial_content",
+                                "user.image","user.bio","user.website","user.tweets",
+                                "user.following","user.media","engagement.rate")
+            )
+
+write.csv(sub,file = "../../datasets/data-prepared.csv",row.names = FALSE)
 
 # TO DO
-#   - compute eng-rate
-#   - remove vars used for eng-rate
-#   - convert some vars into binary
 #   - define functions to count hashtags in a tweet and to get tweet_length
 #   - something else...
+
+tweets_df<-read.csv("../../datasets/data-prepared.csv",sep = ',')
